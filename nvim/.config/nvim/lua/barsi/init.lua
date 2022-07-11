2216d3ff41367f7747ef27f88f80d1393ed66318
w3barsi <darcybalaga@gmail.com>
2022-07-11 22:15:21 +0800

    nvim | autocmd lua

--- nvim/.config/nvim/lua/barsi/init.lua
+++ nvim/.config/nvim/lua/barsi/init.lua
@@ -1,17 +1,20 @@
-require('barsi.lsp.lsp')
-require('barsi.lsp.cmp')
-require('barsi.lsp.null-ls')
-require('barsi.lsp.treesitter')
+require('barsi.autocmd')
+require('barsi.options')
+
 require('barsi.indentline')
 require('barsi.autotag')
 require('barsi.git')
-require('barsi.options')
 require('barsi.code_runner')
 require('barsi.nvim-tree')
 require('barsi.telescope')
 require('barsi.terminal')
 require('barsi.themes')
 require('barsi.transparent')
+
+require('barsi.lsp.lsp')
+require('barsi.lsp.cmp')
+require('barsi.lsp.null-ls')
+require('barsi.lsp.treesitter')
 
 require('nvim-autopairs').setup{}
 
