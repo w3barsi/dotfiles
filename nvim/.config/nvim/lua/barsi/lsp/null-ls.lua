2e07049bcb23756e8058d86f42bd76562e059ea6
w3barsi <darcybalaga@gmail.com>
2022-07-26 19:46:36 +0800

    nvim | less fancy null-ls config

--- nvim/.config/nvim/lua/barsi/lsp/null-ls.lua
+++ nvim/.config/nvim/lua/barsi/lsp/null-ls.lua
@@ -1,15 +1,20 @@
-local null_ls_status_ok, null_ls = pcall(require, "null-ls")
-if not null_ls_status_ok then
-    return
-end
+--local null_ls_status_ok, null_ls = pcall(require, "null-ls")
+--if not null_ls_status_ok then
+    --return
+--end
 
-local formatting = null_ls.builtins.formatting
-local diagnostics = null_ls.builtins.diagnostics
+--local formatting = null_ls.builtins.formatting
+--local diagnostics = null_ls.builtins.diagnostics
 
-null_ls.setup({
-    debug = false,
+--null_ls.setup({
+    --debug = false,
+    --sources = {
+        --formatting.prettier,
+    --}
+--})
+
+require("null-ls").setup({
     sources = {
-        formatting.prettier,
-    }
+        require("null-ls").builtins.formatting.prettier,
+    },
 })
-
