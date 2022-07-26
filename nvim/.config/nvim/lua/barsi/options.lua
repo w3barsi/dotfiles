578126dba5ade8d762c1dd5da8294705eae729aa
w3barsi <darcybalaga@gmail.com>
2022-07-26 21:25:38 +0800

    nvim | removed sets.vim for options.lua

--- nvim/.config/nvim/lua/barsi/options.lua
+++ nvim/.config/nvim/lua/barsi/options.lua
@@ -1,16 +1,54 @@
-local options = {
-  encoding = "UTF-8",
-  clipboard = "unnamedplus",
-  autoread = true,
-  rnu = true,
-  nu = true,
+vim.g.mapleader = " "
+vim.cmd[[colorscheme darkplus]]
+
+vim.opt.encoding = "UTF-8"
+vim.opt.fillchars = {
+    vert = " ",
+    eob = " "
 }
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.hidden = true
+vim.opt.autoread = true
+vim.opt.nu = true
+vim.opt.rnu = true
+vim.opt.hlsearch = false
 
-vim.opt.shortmess:append "c"
+vim.opt.mouse= "a"
+vim.opt.mousefocus = true
 
-for k, v in pairs(options) do
-  vim.opt[k] = v
-end
+vim.opt.encoding= "utf-8"
 
---vim.cmd [[autocmd! CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false, scope="cursor"})]]
--- Diagnostics on Cursor Hold
+vim.opt.errorbells = false
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.wrap = false
+vim.opt.smartcase = true
+vim.opt.ignorecase = true
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir= vim.fn.stdpath("data") .. "undo"
+vim.opt.undofile = true
+vim.opt.incsearch = true
+--vim.opt.colorcolumn=80
+vim.opt.scrolloff=8
+vim.opt.laststatus=2
+vim.opt.signcolumn= "yes:1"
+vim.opt.clipboard = "unnamedplus"
+vim.opt.lazyredraw = true
+vim.opt.updatetime=50
+vim.opt.termguicolors = true
+vim.opt.cursorline = true
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+vim.opt.completeopt = "menu,menuone,noselect"
+
+vim.opt.backspace = "indent,eol,start"
+
+vim.cmd[[highlight GitSignsAdd guibg=NONE]]
+vim.cmd[[highlight GitSignsAdd guifg=#b8bb26]]
+vim.cmd[[highlight GitSignsChange guibg=NONE]]
+vim.cmd[[highlight GitSignsChange guifg=#fe8019]]
+vim.cmd[[highlight GitSignsDelete guibg=NONE]]
+vim.cmd[[highlight GitSignsDelete guifg=#fb4934]]
