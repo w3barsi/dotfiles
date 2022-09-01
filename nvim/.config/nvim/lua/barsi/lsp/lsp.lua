b0a9bfe978bbd5a75d80ee862b2e99a4406b3e6c
darcybalaga@gmail.com <darcybalaga@gmail.com>
2022-09-01 17:35:18 +0800

    moved lsp binds to 'barsi.remap'

--- nvim/.config/nvim/lua/barsi/lsp/lsp.lua
+++ nvim/.config/nvim/lua/barsi/lsp/lsp.lua
@@ -8,45 +8,46 @@ require("nvim-lsp-installer").setup {
   }
 }
 
+local on_attach = require("barsi.remap").on_attach
+
 local saga = require('lspsaga')
 saga.init_lsp_saga({
-  border_style = "solid"
+  border_style = "single"
 })
 
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 
-local open_float = function()
-  local opts = {
-    close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
-    border = 'rounded',
-  }
-  vim.api.nvim_command('lua vim.diagnostic.open_float(nil, {close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" }, border = "rounded",})')
-end
+--local open_float = function()
+  --local opts = {
+    --close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
+    --border = 'rounded',
+  --}
+  --vim.api.nvim_command('lua vim.diagnostic.open_float(nil, {close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" }, border = "rounded",})')
+--end
 
-local on_attach = function()
-  --vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = 0 })
-  vim.keymap.set("n", "K", "<cmd>Lspsaga hover_doc<CR>", { silent = true, buffer = 0 })
+--local on_attach = function()
+  ----vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = 0 })
+  --vim.keymap.set("n", "K", "<cmd>Lspsaga hover_doc<CR>", { silent = true, buffer = 0 })
 
-  vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = 0 })
-  vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, { buffer = 0 })
-  vim.keymap.set("n", "gi", vim.lsp.buf.implementation, { buffer = 0 })
-  vim.keymap.set("n", "gr", vim.lsp.buf.references, { buffer = 0 })
-  vim.keymap.set("n", "gf", vim.lsp.buf.format, { buffer = 0 })
+  --vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = 0 })
+  --vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, { buffer = 0 })
+  --vim.keymap.set("n", "gi", vim.lsp.buf.implementation, { buffer = 0 })
+  --vim.keymap.set("n", "gr", vim.lsp.buf.references, { buffer = 0 })
+  --vim.keymap.set("n", "gf", vim.lsp.buf.format, { buffer = 0 })
 
-  --vim.keymap.set("n", "gca", vim.lsp.buf.code_action, { buffer = 0 })
-  vim.keymap.set("n", "<leader>ca", "<cmd>Lspsaga code_action<CR>", { silent = true })
-  vim.keymap.set("v", "<leader>ca", "<cmd><C-U>Lspsaga range_code_action<CR>", { silent = true })
+  ----vim.keymap.set("n", "gca", vim.lsp.buf.code_action, { buffer = 0 })
+  --vim.keymap.set("n", "<leader>ca", "<cmd>Lspsaga code_action<CR>", { silent = true })
+  --vim.keymap.set("v", "<leader>ca", "<cmd><C-U>Lspsaga range_code_action<CR>", { silent = true })
 
-  --vim.keymap.set("n", "<F2>", vim.lsp.buf.rename, { buffer = 0 })
-  vim.keymap.set("n", "<F2>", "<cmd>Lspsaga rename<CR>", { silent = true, buffer = 0 })
+  ----vim.keymap.set("n", "<F2>", vim.lsp.buf.rename, { buffer = 0 })
+  --vim.keymap.set("n", "<F2>", "<cmd>Lspsaga rename<CR>", { silent = true, buffer = 0 })
 
-  vim.keymap.set("n", "?", open_float, { buffer = 0 })
-  vim.keymap.set("n", "<leader>dj", vim.diagnostic.goto_next, { buffer = 0 })
-  vim.keymap.set("n", "<leader>dk", vim.diagnostic.goto_prev, { buffer = 0 })
-  vim.keymap.set("n", "<leader>dp", "<cmd>Telescope diagnostics<cr>", { buffer = 0 })
-
-end
+  --vim.keymap.set("n", "?", open_float, { buffer = 0 })
+  --vim.keymap.set("n", "<leader>dj", vim.diagnostic.goto_next, { buffer = 0 })
+  --vim.keymap.set("n", "<leader>dk", vim.diagnostic.goto_prev, { buffer = 0 })
+  --vim.keymap.set("n", "<leader>dp", "<cmd>Telescope diagnostics<cr>", { buffer = 0 })
+--end
 
 -- vimls html sumneko_lua bashls yamlls prismals ccls clangd eslint intelephense jsonls tailwindcss tsserver cssls vimls
 
