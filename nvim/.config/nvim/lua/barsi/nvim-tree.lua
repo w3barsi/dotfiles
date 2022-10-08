1a8eb04cca505f8f9fa25684db06b0ac653f7800
w3barsi <darcybalaga@gmail.com>
2022-10-09 00:11:48 +0800

    nvim-tree now shows markers

--- nvim/.config/nvim/lua/barsi/nvim-tree.lua
+++ nvim/.config/nvim/lua/barsi/nvim-tree.lua
@@ -36,7 +36,7 @@ require("nvim-tree").setup({ -- BEGIN_DEFAULT_OPTS
 		highlight_opened_files = "none",
 		root_folder_modifier = ":~",
 		indent_markers = {
-			enable = false,
+			enable = true,
 			icons = {
 				corner = "└ ",
 				edge = "│ ",
