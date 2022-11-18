dc9a40c06c5570613996bde7d7662764e3279c6d
w3barsi <darcybalaga@gmail.com>
2022-11-18 15:10:24 +0800

    made installation automatic via git raw link

--- INSTALL_UBUNTU.sh
+++ INSTALL_UBUNTU.sh
@@ -3,26 +3,26 @@
 RED='\033[0;31m'
 NC='\033[0m' # No Color
 
-cd "/home/barsi"
+sudo apt install nala git -y
+cd
 git clone https://github.com/w3barsi/dotfiles
 rm -rf Desktop Documents Downloads Music Pictures Public Templates Videos
 
 sudo apt update
 sudo apt upgrade -y
-printf "${RED}[-----]${NC} Downloading initial dependencies..."
-sudo apt install nala -y
+printf "${RED}[-----]${NC} Downloading initial dependencies...\n"
 sudo nala install stow zsh zoxide exa curl unzip npm python3 python3-pip fontconfig -y
 
-printf "${RED}[-----]${NC} Installing Starship Prompt"
+printf "${RED}[-----]${NC} Installing Starship Prompt\n"
 curl -sS https://starship.rs/install.sh | sh
 
-printf "${RED}[-----]${NC} Making python3 default python command"
+printf "${RED}[-----]${NC} Making python3 default python command\n"
 sudo update-alternatives --install /usr/bin/python python /usr/bin/python3 10
 
-printf "${RED}[-----]${NC} Installing fnm - A node version manager"
+printf "${RED}[-----]${NC} Installing fnm - A node version manager\n"
 curl -fsSL https://fnm.vercel.app/install | bash
 
-printf "${RED}[-----]${NC} Installing Rust"
+printf "${RED}[-----]${NC} Installing Rust\n"
 curl https://sh.rustup.rs -sSf | sh -s -- -y
 source "/home/barsi/.cargo/env"
 
@@ -36,10 +36,10 @@ source "/home/barsi/.cargo/env"
 # printf "${RED}[-----]${NC} Installed JetBrainsMono"
 
 
-printf "${RED}[-----]${NC} Stowing Ubuntu/WSL dotfiles"
-printf "${RED}[-----]${NC}  - tmux"
-printf "${RED}[-----]${NC}  - zsh"
-printf "${RED}[-----]${NC}  - starship"
+printf "${RED}[-----]${NC} Stowing Ubuntu/WSL dotfiles\n"
+printf "${RED}[-----]${NC}  - tmux\n"
+printf "${RED}[-----]${NC}  - zsh\n"
+printf "${RED}[-----]${NC}  - starship\n"
 cd
 rm -rf .zshrc .starship.toml .tmux.conf
 cd dotfiles
@@ -48,10 +48,10 @@ cd
 
 source "$HOME/.zshrc"
 
-printf "${RED}[-----]${NC} Installing node 18"
+printf "${RED}[-----]${NC} Installing node 18\n"
 fnm install 18
 
-printf "${RED}[-----]${NC} Installing neovim build dependencies"
+printf "${RED}[-----]${NC} Installing neovim build dependencies\n"
 sudo nala install ninja-build \
     gettext \
     libtool \
@@ -66,17 +66,27 @@ sudo nala install ninja-build \
     doxygen -y
 
 cd
-printf "${RED}[-----]${NC} Cloning neovim"
+printf "${RED}[-----]${NC} Cloning neovim\n"
 git clone https://github.com/neovim/neovim
 cd neovim
 git checkout stable
-printf "${RED}[-----]${NC} Checked out to stable branch"
-printf "${RED}[-----]${NC} Now building..."
+printf "${RED}[-----]${NC} Checked out to stable branch\n"
+printf "${RED}[-----]${NC} Now building...\n"
 make CMAKE_BUILD_TYPE=RelWithDebInfo
 sudo make install
 cd
 
-printf "${RED}[-----]${NC} Installing neovim build dependencies"
+printf "${RED}[-----]${NC} Installing neovim IDE dependencies\n"
+printf "${RED}[-----]${NC} - ripgrep\n"
+sudo nala install ripgrep -y
+
+printf "${RED}[-----]${NC} - lazygit\n"
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep '"tag_name":' |  sed -E 's/.*"v*([^"]+)".*/\1/')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+sudo tar xf lazygit.tar.gz -C /usr/local/bin lazygit
+rm -rf lazygit.tar.gz
+
+printf "${RED}[-----]${NC} Installing LunarVim -- Checkhealth Shortcut"
 LV_BRANCH='release-1.2/neovim-0.8' bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh)
 
 cd "/home/barsi/dotfiles"
