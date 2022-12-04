f0ab8420aea373a8c5e213ce1572f63efee5eeed
w3barsi <kuya@Maribels-MacBook-Air.local>
2022-12-04 23:52:36 +0800

    removed unused parts

--- nvim/.config/nvim/lua/barsi/lsp/cmp.lua
+++ nvim/.config/nvim/lua/barsi/lsp/cmp.lua
@@ -6,72 +6,46 @@ if not snip_status_ok then
 	return
 end
 
-require("luasnip/loaders/from_snipmate").lazy_load({ paths = "./snippets" })
-require("luasnip/loaders/from_vscode").lazy_load()
+-- require("luasnip/loaders/from_snipmate").lazy_load({ paths = "./snippets" })
+-- require("luasnip/loaders/from_vscode").lazy_load()
 
-local kind_icons = {
-	Text = "",
-	Method = "",
-	Function = "",
-	Constructor = "",
-	Field = "ﰠ",
-	Variable = "",
-	Class = "ﴯ",
-	Interface = "",
-	Module = "",
-	Property = "ﰠ",
-	Unit = "塞",
-	Value = "",
-	Enum = "",
-	Keyword = "",
-	Snippet = "",
-	Color = "",
-	File = "",
-	Reference = "",
-	Folder = "",
-	EnumMember = "",
-	Constant = "",
-	Struct = "פּ",
-	Event = "",
-	Operator = "",
-	TypeParameter = "",
-}
-
-
+local has_words_before = function()
+	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
 
 cmp.setup({
 	snippet = {
 		expand = function(args)
-			--vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
-			luasnip.lsp_expand(args.body) -- For `luasnip` users.
+			luasnip.lsp_expand(args.body)
 		end,
 	},
 	--window = {
 	----completion = { border = border,},
 	----documentation = { border = border },
 	--},
+
 	mapping = {
-		["<C-d>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
-		["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
 		["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
+
 		["<C-y>"] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
 		["<C-e>"] = cmp.mapping({
 			i = cmp.mapping.abort(),
 			c = cmp.mapping.close(),
 		}),
 		["<CR>"] = cmp.mapping.confirm(),
-		["<Tab>"] = function(fallback)
+		["<Tab>"] = cmp.mapping(function(fallback)
 			if cmp.visible() then
 				cmp.select_next_item()
-			elseif luasnip.expandable() then
-				luasnip.expand()
 			elseif luasnip.expand_or_jumpable() then
 				luasnip.expand_or_jump()
+			elseif has_words_before() then
+				cmp.complete()
 			else
 				fallback()
 			end
-		end,
-		["<S-Tab>"] = function(fallback)
+		end, { "i", "s" }),
+		["<S-Tab>"] = cmp.mapping(function(fallback)
 			if cmp.visible() then
 				cmp.select_prev_item()
 			elseif luasnip.jumpable(-1) then
@@ -79,16 +53,15 @@ cmp.setup({
 			else
 				fallback()
 			end
-		end,
+		end, { "i", "s" }),
 	},
 	sources = cmp.config.sources(
 		{
 			{ name = "nvim_lsp_signature_help" },
-			{ name = "nvim_lsp" },
 			{ name = "luasnip" },
+			{ name = "nvim_lsp" },
 			{ name = "buffer" },
 			{ name = "path" },
-			--{ name = 'ultisnips' }, -- For ultisnips users.
 		}
 		--{
 		--{ name = 'buffer' },
@@ -151,3 +124,35 @@ cmp.setup.cmdline(":", {
 ----ultisnips = "[SNIP]",
 --}
 --})
+--
+
+-- local kind_icons = {
+-- 	Text = "",
+-- 	Method = "",
+-- 	Function = "",
+-- 	Constructor = "",
+-- 	Field = "ﰠ",
+-- 	Variable = "",
+-- 	Class = "ﴯ",
+-- 	Interface = "",
+-- 	Module = "",
+-- 	Property = "ﰠ",
+-- 	Unit = "塞",
+-- 	Value = "",
+-- 	Enum = "",
+-- 	Keyword = "",
+-- 	Snippet = "",
+-- 	Color = "",
+-- 	File = "",
+-- 	Reference = "",
+-- 	Folder = "",
+-- 	EnumMember = "",
+-- 	Constant = "",
+-- 	Struct = "פּ",
+-- 	Event = "",
+-- 	Operator = "",
+-- 	TypeParameter = "",
+-- }
+--
+
+
